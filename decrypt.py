6d237389cf0b60c972dc04d47572a60cd7b97caf
GabinChautemps <gabin.chautemps06@gmail.com>
2024-06-23 16:26:45 +0200

    update done by GabinChautemps

--- decrypt.py
+++ decrypt.py
@@ -5,17 +5,15 @@ from enigma.machine import EnigmaMachine
 rotinput=input("Quelle est la position des rotors ? (exemple : IV I V)\n")
 
 # Boucle, pour les rotors, pour que l'utilisateur entre forcément une valeur 
-if rotinput == "":
-    while rotinput == "":
-        rotinput=input("Entrez impérativement la postition des rotor !!!\n")
+while rotinput == "":
+    rotinput=input("Entrez impérativement la postition des rotor !!!\n")
 
 # Demande les position des anneaux à l'utilisateur
 ringinput=input("Quelles sont la valeure des anneaux ? (exemple : 20 5 10)\n")
 
 # Boucle, pour les anneaux, pour que l'utilisateur entre forcément une valeur
-if ringinput == "":
-    while rotinput == "":
-        rotinput=input("Entrez impérativement la valeure des anneaux !!!\n")
+while rotinput == "":
+    rotinput=input("Entrez impérativement la valeure des anneaux !!!\n")
 
 # Demande les 10 pairs du tableau de connexion à l'utilisateur
 plugboardinput=input("Quelles sont les 10 paires du tableau de connexion ?\nSi vous mettez rien, ces paramêtre seront utiliser : SX KU QP VN JG TC LA WM OB ZF\n")
@@ -28,7 +26,7 @@ if plugboardinput == "":
 machine = EnigmaMachine.from_key_sheet(
     rotors=(rotinput),
     reflector='B',
-    ring_settings=(ringinput),
+    ring_settings=[int(n) for n in ringinput.split()], # Converti "string" (entrée utilisateur) en "int"
     plugboard_settings=(plugboardinput))
 
 # Demande a l'utilisateur la position initale des rotors
@@ -36,9 +34,8 @@ displayinput=input("Quelle est la position initale des rotors ? (exemple FNZ)\n"
 
 # Boucle, pour la position initale des rotors,
 # pour que l'utilisateur entre forcément une valeur
-if displayinput == "":
-    while displayinput == "":
-        displayinput=input("Entrez impérativement la postition initiale des rotors !!!\n")
+while displayinput == "":
+    displayinput=input("Entrez impérativement la postition initiale des rotors !!!\n")
 
 # Mise en place de la position initiale des rotors
 machine.set_display(displayinput)
@@ -47,7 +44,7 @@ machine.set_display(displayinput)
 # et l'enregistre dans un fonction msg_key
 cle = input('Quelle est votre clé pour décrypter le texte ?\n')
 msg_key = machine.process_text(cle)
-print(msg_key)
+print(f"La clé de message est : {msg_key}")
 
 # Règle la nouvelle postion des rotors
 machine.set_display(msg_key)
@@ -60,8 +57,3 @@ print(f'Voici le texte décodé : {plaintext}')
 
 # Message de fin du programme
 print("Merci d'avoir utilisé notre programme !")
-
-
-
-
-
